c1c74444f3018be96be10bc96e64d246af583a98
James Williams <james@jameswilliams.me>
2023-12-01 15:18:58 -0500

    Day 1, For Some Reason

---
+++ day1/day1.fsx
@@ -0,0 +1,87 @@
+let input =
+  match fsi.CommandLineArgs with
+  | [| _; filename |] -> System.IO.File.ReadAllLines(filename)
+  | _ -> System.IO.File.ReadAllLines("sample.txt")
+  |> Array.toList
+
+let firstDigitSimple (line: string) =
+  line
+  |> Seq.find (System.Char.IsDigit)
+  |> string
+
+let lastDigitSimple (line: string) =
+  line
+  |> Seq.rev
+  |> Seq.find (System.Char.IsDigit)
+  |> string
+
+let allSubstringsUpToLength (l: int) (s: string) = seq {
+  let stringLength = s.Length
+  for i = 0 to (stringLength) do
+    for l' = 1 to l do
+      if i + l' <= stringLength then
+        yield s.Substring(i, l')
+}
+
+let allDigitsComplex (line: string) =
+  let possibleDigits =
+    let addStringToMap (s: string) v m =
+      m
+      |> Map.add s {|Length = s.Length; Value = v |}
+
+    Map.empty
+    |> addStringToMap "1" 1
+    |> addStringToMap "2" 2
+    |> addStringToMap "3" 3
+    |> addStringToMap "4" 4
+    |> addStringToMap "5" 5
+    |> addStringToMap "6" 6
+    |> addStringToMap "7" 7
+    |> addStringToMap "8" 8
+    |> addStringToMap "9" 9
+    |> addStringToMap "0" 0
+    |> addStringToMap "one" 1
+    |> addStringToMap "two" 2
+    |> addStringToMap "three" 3
+    |> addStringToMap "four" 4
+    |> addStringToMap "five" 5
+    |> addStringToMap "six" 6
+    |> addStringToMap "seven" 7
+    |> addStringToMap "eight" 8
+    |> addStringToMap "nine" 9
+    |> addStringToMap "ten" 0
+
+  let longestPossibleDigit =
+    possibleDigits
+    |> Map.values
+    |> Seq.map (fun v -> v.Length)
+    |> Seq.max
+
+  line
+    |> allSubstringsUpToLength longestPossibleDigit
+    |> Seq.map (fun s -> possibleDigits |> Map.tryFind s)
+    |> Seq.choose id
+    |> Seq.map (fun v -> v.Value)
+    |> Seq.toList
+
+
+let part1 lines =
+  let makeInt (firstDigit, lastDigit) = (System.Int32.Parse(firstDigit) * 10) + System.Int32.Parse(lastDigit)
+
+  lines
+  |> List.map (fun line -> ((firstDigitSimple line), (lastDigitSimple line)))
+  |> List.map makeInt
+  |> List.sum
+  |> sprintf "%A"
+
+let part2 lines =
+  lines
+  |> List.map (fun l ->
+      let digits = allDigitsComplex l
+      (digits |> List.head, digits |> List.rev |> List.head ))
+  |> List.map (fun (first, last) -> (first * 10) + last)
+  |> List.sum
+  |> string
+
+part1 input |> printfn "%s"
+part2 input |> printfn "%s"
